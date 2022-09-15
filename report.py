36100bd3b775bbb55977ce5a87c98aab79322f81
ryum_chan <98261891+natiryum@users.noreply.github.com>
2022-09-15 17:45:21 +0900

    Add files via upload

---
+++ report.py
@@ -0,0 +1,8 @@
+#1주차부터 50주차까지의 보고서 파일 제작
+
+for i in range (1,51):
+    with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
+        report_file.write("- {0} 주차 주간보고 -".format(i))
+        report_file.write("\n부서 :")
+        report_file.write("\n이름 :")
+        report_file.write("\n업무 요약 :")
